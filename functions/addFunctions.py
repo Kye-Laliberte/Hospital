9a7589773daeaeb878559aa867cf69ef2e5a485b
Kye Laliberte <klaliber@icloud.com>
2025-11-02 21:27:59 -0600

    This adds som small err traping

--- functions/addFunctions.py
+++ functions/addFunctions.py
@@ -5,7 +5,17 @@ def addMedication(name,dosage,MedicationType,db_path="app.db"):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     try:
-        
+        #finds if the med is alredy in use
+        cursor.execute("""
+            SELECT Med_id FROM Medication
+            WHERE name = ? AND dosage = ? AND MedicationType = ?;
+        """, (name, dosage, MedicationType))
+        existing = cursor.fetchone()
+
+        if existing:
+            print(f"Medication '{name} {dosage}{MedicationType}' already exists (ID: {existing[0]}).")
+            return 
+
         cursor.execute("""
         INSERT INTO Medication (name,dosage,MedicationType)
         VALUES(?,?,?);""",(name,dosage,MedicationType))
@@ -25,12 +35,25 @@ def addDoctor(first_name, last_name, department, phone, db_path="app.db"):
     """adds a new Doctor to the Doctors table"""
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
+    # need to a dupicat Docter detecter
     try:
+        # Check if doctor already exists
+        cursor.execute("""
+            SELECT doctor_id FROM Doctors
+            WHERE first_name = ? AND last_name = ? AND department = ?;
+        """, (first_name, last_name, department))
+        location = cursor.fetchone()
+        
+        if location:
+            print(f"Doctor {first_name} {last_name} already exists (ID: {location[0]}).")
+            return 
+
         cursor.execute("""
             INSERT INTO Doctors (first_name, last_name, department, phone)
             VALUES (?, ?, ?, ?);
         """, (first_name, last_name, department, phone))
         conn.commit()
+
         print(f"Doctor {first_name} {last_name} added successfully.")
     except sqlite3.Error as e:
         print(f"Database error: {e}")
@@ -42,7 +65,23 @@ def addPatient(gender, first_name, last_name, dob, email, phone, address, status
     """adds a Patient to the Patients table"""
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
+
+    #need to add a duplicat Patient dettecter
     try:
+
+         # Check if Patient already exists
+        cursor.execute("""
+            SELECT doctor_id FROM Patients
+            WHERE first_name = ? AND last_name = ? AND gender = ?;
+        """, (first_name, last_name, gender))
+        location = cursor.fetchone()
+        
+        if location:
+            print(f"Patients {first_name} {last_name} already exists (ID: {location[0]}).")
+            return 
+
+
+
         cursor.execute("""
             INSERT INTO Patients (gender, first_name, last_name, date_of_birth, email, phone, address, STATUS)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?);
